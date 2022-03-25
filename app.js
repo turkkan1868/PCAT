f04a355224cde3cb136d33877778d507fe09ae6d
turkkan1868 <99186587+turkkan1868@users.noreply.github.com>
2022-03-25 17:46:31 +0300

    sonves

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express');
-
+const path = require('path')
 const app = express();
+app.use(express.static('public'));
+
+// const myLogger = (req, res, next) => {
+//   console.log('Middleware Log 1');
+//   next();
+// }
+
+
+
+app.use(express.static('public'));
+// app.use(myLogger);
+
 
 app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
 
+
+
+app.get('/', (req, res) => {
   const photo = {
     id: 1,
     name: "Photo Name",
